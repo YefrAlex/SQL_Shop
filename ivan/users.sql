ee45401d12da4c942a252312823567db3f28d858
ikar-zindo <plag.ikar@gmail.com>
2023-11-07 11:27:02 +0100

    add create_at TIMESTAMP

--- ivan/users.sql
+++ ivan/users.sql
@@ -3,7 +3,7 @@ USE `shop`;
 
 CREATE TABLE `users` (
    `id` INT NOT NULL PRIMARY KEY,
-   `created_at` DEFAULT CURRENT_TIMESTAMP,
+   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
    `fullname` VARCHAR(32) NOT NULL,
    `email` VARCHAR(32) NOT NULL,
    `country` VARCHAR(32),
