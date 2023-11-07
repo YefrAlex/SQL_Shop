23551d0f17f693b97d1e8a31e2411c8e12626097
ikar-zindo <plag.ikar@gmail.com>
2023-11-07 10:56:19 +0100

    add value NOT NULL

--- ivan/users.sql
+++ ivan/users.sql
@@ -4,9 +4,9 @@ USE `shop`;
 CREATE TABLE `categories` (
    `id` INT NOT NULL PRIMARY KEY,
    `created_at` DEFAULT CURRENT_TIMESTAMP,
-   `fullname` VARCHAR(32),
-   `email` VARCHAR(32),
+   `fullname` VARCHAR(32) NOT NULL,
+   `email` VARCHAR(32) NOT NULL,
    `country` VARCHAR(32),
-   `balance` INT DEFAULT 0,
+   `balance` NOT NULL INT DEFAULT 0,
    `is_blocked` BOOLEAN DEFAULT FALSE
 )
