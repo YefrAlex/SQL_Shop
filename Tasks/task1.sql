d315f8a2fa08f26d22b2c8c8227cb14fd05482f4
Yefremenko Alexei <aresboo@gmail.con>
2023-11-07 11:30:45 +0100

    create new table - users

--- Tasks/task1.sql
+++ Tasks/task1.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS users (
- id int [primary key, increment],
- created_at timestamp [default: `now()`],
- fullname varchar NOT_NULL,
- email varchar NOT_NULL,
+ id serial primary key,
+ created_at timestamp default now(),
+ fullname varchar NOT NULL,
+ email varchar NOT NULL,
  country varchar,
- balance int bool [default: 0],
- is_blocked bool [default: `false`]
+ balance int default 0,
+ is_blocked bool default false
 );
 
